aa6fcd75d1d3b5748a8704df5c10044a34e102a7
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-04-25 06:21:37 +0700

    fix dynamic naming conflict

--- app/mode/page.tsx
+++ app/mode/page.tsx
@@ -1,8 +1,8 @@
-import dynamic from "next/dynamic";
+import nextDynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
 
-const ModeInner = dynamic(
+const ModeInner = nextDynamic(
   () => import("./ModeInner"),
   { ssr: false }
 );
