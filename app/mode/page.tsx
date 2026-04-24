f236de5cb4464f2d3617a27657c3a198d4c18ae1
sunoramabatu@gmail.com <emailanda@gmail.com>
2026-04-24 21:40:19 +0700

    fix suspense boundary mode page

--- app/mode/page.tsx
+++ app/mode/page.tsx
@@ -1,14 +1,5 @@
-"use client";
+import ModeClient from "./mode-client";
 
-import { Suspense } from "react";
-import ModeInner from "./ModeInner";
-
-export const dynamic = "force-dynamic";
-
-export default function ModePage() {
-  return (
-    <Suspense fallback={<div>Memuat mode...</div>}>
-      <ModeInner />
-    </Suspense>
-  );
+export default function Page() {
+  return <ModeClient />;
 }
